35bcc01b84f5c61edca72072d9a8d7c20cb55148
Tamzidul Matin <41488579+tmatin100@users.noreply.github.com>
2022-02-11 09:34:59 -0500

    Add files via upload

---
+++ 25-funciton_example.sh
@@ -0,0 +1,26 @@
+#! /bin/bash 
+# This example shell script will demonstrate the concept of Bash shell functions. 
+# we will show to check if a file exists or not using function.
+
+# lets create a function called usage
+usage(){
+    echo "You need to provide an aurgument: "
+    echo "usage: $0 file_name"
+}
+
+# we can get 1 if the file exists , and 0 if it doesnt 
+is_file_exist(){
+    local file="$1"
+    [[ -f "$file" ]] && return 0 || return 1 
+}
+
+# if the total number of aurgument is equal to 0 then execute the usage fucntion 
+[[ $# -eq 0  ]] && usage 
+
+if ( is_file_exist "$1" )
+then
+  echo "File is found"
+else 
+  echo "File is not found"
+fi
+
