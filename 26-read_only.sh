ef0d85cd7bac91b8fda71b82a3790e260310383a
Tamzidul Matin <41488579+tmatin100@users.noreply.github.com>
2022-02-11 10:10:42 -0500

    Add files via upload

--- 26-read_only.sh
+++ 26-read_only.sh
@@ -1,2 +1,37 @@
 #! /bin/bash 
 
+# readonly command can be used to to make you variables and functions read only 
+# which means you cannot change the value of variables or you can not overwrite a function
+
+var=21
+#make the variable readonly
+readonly var
+var=50
+
+echo "var => $var"
+
+
+#creatae and call a function 
+hello(){
+   echo "hello World"
+}
+
+hello
+
+# we can use the -f flag to make a function read only 
+readonly -f hello
+hello(){
+    echo "hello Wolrd Again"
+}
+
+hello
+
+#check all the readonly variables that are there by default
+echo "----------------------------------------readonly variables------------------------------------------------------------------------"
+readonly 
+
+#list all the read only varuabkes 
+readonly -p
+
+#list of read only functions
+readonly -f
